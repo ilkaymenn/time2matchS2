7b6bb194eda59f85f489f9de9446239232eddb39
MeldryckSAID <meldryck.said@edu.univ-fcomte.fr>
2022-05-24 15:41:03 +0200

    commit test

--- time2match/src/views/AccueilView.vue
+++ time2match/src/views/AccueilView.vue
@@ -1,9 +1,9 @@
-<template>
-  <div class="container ">                 
-    
+<template >
+  <div class="container bg-slate-900 ">                 
+    <p>test</p>
   </div>
 </template>
-
+  
 <script> 
 
 export default {
